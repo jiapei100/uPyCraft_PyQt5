67c5329e9ea79915c9a73b961acf5d8335c777fc
jiapei100 <jp4work@gmail.com>
2018-09-16 20:29:01 -0700

    Add OpenMV

--- updateNewFirmware.py
+++ updateNewFirmware.py
@@ -143,7 +143,7 @@ class updateNewFirmware(QDialog):
 
         self.boardLabel=QLabel(self.tr("board"))
         self.boardComboBox=QComboBox()
-        self.boardComboBox.addItems(["esp8266","TPYBoardV202","esp32","microbit"])
+        self.boardComboBox.addItems(["esp8266","TPYBoardV202","esp32","microbit","OpenMV"])
         self.boardComboBox.setCurrentIndex(2)
 
         self.burnAddrLabel=QLabel(self.tr("burn_addr"))
